08ea057a6b26416a438eaf586d95a781acc685b5
Nafiyad Adane <nafiadg@gmail.com>
2025-04-08 12:56:17 -0600

    added jenkins

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import './App.css';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Nafiyad heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Nafiyad/i);
+  expect(headingElement).toBeInTheDocument();
 });
